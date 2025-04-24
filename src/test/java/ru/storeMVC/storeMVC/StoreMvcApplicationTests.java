178b7b2620aef0217cef6cbaedf859525c050bbd
пк <mr.setevik@rambler.ru>
2025-04-24 20:49:40 +0300

    Fatal

--- src/test/java/ru/storeMVC/storeMVC/StoreMvcApplicationTests.java
+++ src/test/java/ru/storeMVC/storeMVC/StoreMvcApplicationTests.java
@@ -1,13 +1,14 @@
 package ru.storeMVC.storeMVC;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc(printOnlyOnFailure = false)
 class StoreMvcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
